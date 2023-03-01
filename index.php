b1d871337c26caf5e432b9e396639183c08911be
Daan Reuvers <77575704+DaanRVRS@users.noreply.github.com>
2023-03-01 11:16:29 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='utf-8'>
+        <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+        <title>CineTriplets</title>
+        <meta name='viewport' content='width=device-width, initial-scale=1'>
+        <link rel='stylesheet' type='text/css' media='screen' href='css/index.css'>
+        <script src='main.js'></script>
+        <script src="https://kit.fontawesome.com/aa0c361891.js" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <header>
+            <i class="fa-solid fa-bars fa-2x"></i>
+            <img src="img/CineTriplets.png" width="330px" height="330px">
+            <i class="fa-solid fa-user fa-2x"></i>
+        </header>
+        <!-- bar under header (current location etc.) -->
+        <section class="locationBar">
+            <i class="fa-solid fa-house"></i>
+            <p>> Home</p>
+        </section>
+
+
+        <main>
+
+        </main>
+
+        <footer>
+
+        </footer>
+
+    </body>
+</html>
